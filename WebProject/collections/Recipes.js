5a170dfd82f661cde81233170c3471a001498bd5
JuanChacon <virgo_gt@hotmail.com>
2017-05-31 20:05:22 -0600

    Creacion de las propiedades del schema

--- WebProject/collections/Recipes.js
+++ WebProject/collections/Recipes.js
@@ -1,1 +1,16 @@
 Recipes = new Meteor.Collection('recipes');
+
+RecipeSchema = new SimpleSchema({
+  name: {
+    type: String,
+    label: "Name"
+
+  }
+
+  desc: {
+    type: String,
+    label: "Description"
+  }
+
+
+});
