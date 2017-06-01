fdb9c79051f5c8bbe59cffea7e89d902458c738f
JuanChacon <virgo_gt@hotmail.com>
2017-05-31 20:12:59 -0600

    Creacion de mas propiedades del schema capitulo 6

--- WebProject/collections/Recipes.js
+++ WebProject/collections/Recipes.js
@@ -5,12 +5,29 @@ RecipeSchema = new SimpleSchema({
     type: String,
     label: "Name"
 
-  }
+  },
 
   desc: {
     type: String,
     label: "Description"
+  },
+
+  author: {
+    type: String,
+    label: "Author",
+    autoValue: function() {
+      return this.userId
+    }
+  },
+  createdAt: {
+    type: Date,
+    label: "Created At",
+    autoValue: function() {
+        return new Date()
+    }
   }
+
+
 
 
 });
